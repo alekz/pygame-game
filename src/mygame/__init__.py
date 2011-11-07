9bdde32dcb87941ae4997a9bd2853e0fd65bb366
Alexander Konstantinov <konstantinov@gmail.com>
2011-11-07 18:23:31 +0200

    Changed code structure

---
+++ src/mygame/__init__.py
@@ -0,0 +1,148 @@
+import random
+
+import pygame
+
+from mygame import player
+from mygame.map import Map, RandomMapGenerator
+
+class BaseGame(object):
+
+    def __init__(self):
+
+        self.is_running = False
+
+        self.fps = 60
+        self.screen_size = (640, 480)
+
+        self.clock = None
+        self.screen = None
+
+        self.on_init()
+
+    def start(self):
+
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(self.screen_size)
+
+        self.on_start()
+
+        self.run()
+
+    def run(self):
+        self.is_running = True
+        while True:
+            milliseconds = self.clock.tick(self.fps)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.stop()
+                elif event.type == pygame.KEYDOWN:
+                    self.on_keydown(event.key)
+                elif event.type == pygame.KEYUP:
+                    self.on_keyup(event.key)
+                elif event.type == pygame.MOUSEMOTION:
+                    self.on_mouse_motion(event.pos, event.rel)
+            if self.is_running:
+                self.on_update(milliseconds)
+                self.on_draw(milliseconds)
+                pygame.display.update()
+            else:
+                break
+
+    def stop(self):
+        if self.is_running:
+            self.on_stop()
+            pygame.quit()
+            self.is_running = False
+            self.on_stopped()
+
+    def on_init(self): pass
+
+    def on_start(self): pass
+
+    def on_stop(self): pass
+
+    def on_stopped(self): pass
+
+    def on_keydown(self, key): pass
+
+    def on_keyup(self, key): pass
+
+    def on_mouse_motion(self, pos, rel): pass
+
+    def on_update(self, milliseconds): pass
+
+    def on_draw(self, milliseconds): pass
+
+class Game(BaseGame):
+
+    def on_init(self):
+
+        self.cell_size = (20, 20)
+
+        # Init map
+        map_size = tuple(int(self.screen_size[i] / self.cell_size[i]) for i in (0, 1))
+        map_generator = RandomMapGenerator()
+        self.map = Map(self, map_size, map_generator)
+
+        # Init player
+        self.player = player.Player(self, player.controls.HumanPlayerControls(self))
+        self.player.location = [int(self.map.size[0] / 2), int(self.map.size[1] / 2)]
+        self.map.set_cell(self.player.location, Map.CELL_TYPE_FLOOR) # Initial player's position should be empty
+
+        # Init monsters
+        self.monsters = []
+        for _ in xrange(4):
+            monster = player.Player(self, player.controls.RandomMovementControls(self))
+            monster.location = (random.randint(0, self.map.size[0] - 1), random.randint(0, self.map.size[1] - 1))
+            self.map.set_cell(monster.location, Map.CELL_TYPE_FLOOR) # Initial monster's position should be empty
+            self.monsters.append(monster)
+
+
+    def on_update(self, milliseconds):
+        self.player.update(milliseconds)
+        for monster in self.monsters:
+            monster.update(milliseconds)
+
+    def on_draw(self, milliseconds):
+        self._clear_screen()
+        self._draw_map()
+        self._draw_monsters()
+        self._draw_player()
+        self._draw_fps()
+
+    def _clear_screen(self):
+        self.screen.fill((0, 0, 0))
+
+    def _draw_map(self):
+        for x in xrange(self.map.size[0]):
+            for y in xrange(self.map.size[1]):
+                cell_type = self.map.cells[x][y]
+                color = self.map.colors[cell_type]
+                self._paint_cell(color, (x, y))
+
+    def _draw_player(self):
+        color = (0, 192, 0)
+        self._paint_cell(color, self.player.location, self.player.offset)
+
+    def _draw_monsters(self):
+        color = (192, 0, 0)
+        for monster in self.monsters:
+            self._paint_cell(color, monster.location, monster.offset)
+
+    def _draw_fps(self):
+        #fps = self.clock.get_fps()
+        #print fps
+        pass
+
+    def _paint_cell(self, color, coord, offset=(0.0, 0.0)):
+        rect = pygame.rect.Rect((coord[0] + offset[0]) * self.cell_size[0],
+                                (coord[1] + offset[1]) * self.cell_size[1],
+                                self.cell_size[0],
+                                self.cell_size[1])
+        pygame.draw.rect(self.screen, color, rect)
+
+    def on_keydown(self, key):
+        if key == pygame.K_ESCAPE:
+            self.stop()
--- src/mygame/player/controls.py
+++ src/mygame/player/controls.py
@@ -1,6 +1,8 @@
 import random
+
 import pygame
-from game.types import direction
+
+from mygame.types import direction
 
 class PlayerControls(object):
 
@@ -96,86 +98,3 @@ class RandomMovementControls(PlayerControls):
             self.last_direction = new_direction
 
         return new_direction
-
-class Player(object):
-
-    def __init__(self, game, controls):
-        self.game = game
-        self.direction = direction.NONE
-        self.location = (0, 0) # Cell coordinates within a map
-        self.offset = (0, 0) # Offset within a cell, -1.0..1.0
-        self.speed = 5.0 # Cells per second
-
-        if controls is None:
-            controls = NoMovementControls()
-        self.set_controls(controls)
-
-    def set_controls(self, controls):
-        controls.set_player(self)
-        self.controls = controls
-
-    def set_location(self, coord):
-        self._location = [coord[0], coord[1]]
-    def get_location(self):
-        return tuple(self._location)
-    location = property(get_location, set_location)
-
-    def set_offset(self, coord):
-        self._offset = [float(coord[0]), float(coord[1])]
-    def get_offset(self):
-        return tuple(self._offset)
-    offset = property(get_offset, set_offset)
-
-    def update(self, milliseconds):
-
-        self.controls.update(milliseconds)
-
-        new_direction = self.controls.get_movement_direction()
-
-        if new_direction != direction.NONE:
-
-            if self.direction == direction.NONE:
-                # Start moving, but first check if it is allowed
-                new_location = self._location[:]
-                i, k = direction.get_info(new_direction)
-                new_location[i] += k
-                if not self.game.map.can_move_to(new_location):
-                    new_direction = direction.NONE
-                else:
-                    self.direction = new_direction
-
-            elif direction.is_opposite(new_direction, self.direction):
-                # Move to the opposite direction (should always be allowed)
-                i, k = direction.get_info(self.direction)
-                self._location[i] += k
-                self._offset[i] -= k
-                self.direction = new_direction
-
-        # Move to one of directions
-        if self.direction != direction.NONE:
-
-            i, k = direction.get_info(self.direction)
-
-            # Move a bit
-            self._offset[i] += k * self.speed * milliseconds / 1000.0
-
-            # Check if moved outside of the current cell
-            if abs(self._offset[i]) >= 1.0:
-
-                # Moved to a neighbouring cell
-                self._location[i] += k
-                self._offset[i] -= k
-
-                # Check if further movement is possible: stop if key is not
-                # pressed anymore or if there is an obstacle
-                stop = False
-                if new_direction == direction.NONE:
-                    stop = True
-                else:
-                    new_location = self._location[:]
-                    new_location[i] += k
-                    if not self.game.map.can_move_to(new_location):
-                        stop = True
-                if stop:
-                    self._offset[i] = 0.0
-                    self.direction = direction.NONE
