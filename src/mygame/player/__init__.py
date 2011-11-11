dffe907034fb54bcfce6fd803376e2fc76dc2cfc
Alexander Konstantinov <konstantinov@gmail.com>
2011-11-11 18:35:41 +0100

    Some cleanup of Player class' interface (added properties)

--- src/mygame/player/__init__.py
+++ src/mygame/player/__init__.py
@@ -7,12 +7,13 @@ from mygame.types import direction
 class Player(object):
 
     def __init__(self, game, controls):
-        self.game = game
-        self.direction = direction.NONE
+        self._game = game
+        self._map = game.map
+        self._direction = direction.NONE
+        self._speed = 5.0 # Cells per second
+        self._location_changed = False
         self.location = (0, 0) # Cell coordinates within a map
         self.offset = (0, 0) # Offset within a cell, -1.0..1.0
-        self.speed = 5.0 # Cells per second
-        self.location_changed = False
 
         if controls is None:
             controls = controls.NoMovementControls()
@@ -22,55 +23,75 @@ class Player(object):
         controls.set_player(self)
         self.controls = controls
 
-    def set_location(self, coord):
-        self._location = [coord[0], coord[1]]
-    def get_location(self):
+    @property
+    def location(self):
         return tuple(self._location)
-    location = property(get_location, set_location)
 
-    def set_offset(self, coord):
-        self._offset = [float(coord[0]), float(coord[1])]
-    def get_offset(self):
+    @location.setter
+    def location(self, coord):
+        self._location = [coord[0], coord[1]]
+
+    @property
+    def offset(self):
         return tuple(self._offset)
-    offset = property(get_offset, set_offset)
+
+    @offset.setter
+    def offset(self, coord):
+        self._offset = [float(coord[0]), float(coord[1])]
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, speed):
+        self._speed = float(speed)
+
+    @property
+    def location_changed(self):
+        return self._location_changed
+
+    @property
+    def direction(self):
+        return self._direction
 
     def update(self, milliseconds):
 
-        self.location_changed = False
+        self._location_changed = False
 
         new_direction = self.controls.get_movement_direction()
 
         if new_direction != direction.NONE:
 
-            if self.direction == direction.NONE:
+            if self._direction == direction.NONE:
                 # Start moving, but first check if it is allowed
                 new_location = self._location[:]
                 i, k = direction.get_info(new_direction)
                 new_location[i] += k
-                if not self.game.map.can_move_to(new_location):
+                if not self._map.can_move_to(new_location):
                     new_direction = direction.NONE
                 else:
-                    self.direction = new_direction
+                    self._direction = new_direction
 
-            elif direction.is_opposite(new_direction, self.direction):
+            elif direction.is_opposite(new_direction, self._direction):
                 # Move to the opposite direction (should always be allowed)
-                i, k = direction.get_info(self.direction)
+                i, k = direction.get_info(self._direction)
                 self._location[i] += k
                 self._offset[i] -= k
-                self.direction = new_direction
+                self._direction = new_direction
 
         # Move to one of directions
-        if self.direction != direction.NONE:
+        if self._direction != direction.NONE:
 
-            i, k = direction.get_info(self.direction)
+            i, k = direction.get_info(self._direction)
 
             # Move a bit
-            self._offset[i] += k * self.speed * milliseconds / 1000.0
+            self._offset[i] += k * self._speed * milliseconds / 1000.0
 
             # Check if moved outside of the current cell, i.e. player's location has changed
             if abs(self._offset[i]) >= 1.0:
 
-                self.location_changed = True
+                self._location_changed = True
 
                 # Moved to a neighbouring cell
                 self._location[i] += k
@@ -84,8 +105,8 @@ class Player(object):
                 else:
                     new_location = self._location[:]
                     new_location[i] += k
-                    if not self.game.map.can_move_to(new_location):
+                    if not self._map.can_move_to(new_location):
                         stop = True
                 if stop:
                     self._offset[i] = 0.0
-                    self.direction = direction.NONE
+                    self._direction = direction.NONE
